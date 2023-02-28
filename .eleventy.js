6e1d2bb04d83c1f879d66a28a377ed13fcdd62d1
dustin-jw <dustin@heysparkbox.com>
2023-02-28 09:53:50 -0700

    feat: enable webc components in non-webc templates

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const pluginWebc = require('@11ty/eleventy-plugin-webc');
+const { EleventyRenderPlugin } = require('@11ty/eleventy');
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ 'src/public': '/' });
@@ -7,6 +8,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginWebc, {
     components: 'src/components/**/*.webc',
   });
+  eleventyConfig.addPlugin(EleventyRenderPlugin);
 
   return {
     dir: {
