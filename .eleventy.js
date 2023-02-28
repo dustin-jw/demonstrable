68cb43177d4323b4d967bc9b87dc963a4383bff4
dustin-jw <82047862+dustin-jw@users.noreply.github.com>
2023-02-28 11:05:16 -0700

    feat: slide component (#3)
    * chore: install eleventy-plugin-webc
    
    * chore: configure webc components for global use
    
    * feat: create basic slide component
    
    * feat: use slide component on home page as a test
    
    * feat: enable webc components in non-webc templates
    
    * feat: create some slides to test with
    
    * feat: create a filter to sort slides by order
    
    * feat: render slides in collection in order
    
    * refactor: remove webc configuration stuff
    
    * chore: uninstall eleventy-plugin-webc
    
    * refactor: prefix slides with numbers for easier navigation
    
    * feat: add basic styling for slides
    
    * feat: use scroll snapping to move between slides
    
    * feat: exclude slides from the sitemap

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,19 @@
+function sortByOrder(values) {
+  const vals = [...values];
+  return vals.sort((a, b) => Math.sign(a.data.order - b.data.order));
+}
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ 'src/public': '/' });
   eleventyConfig.addLayoutAlias('default', 'layout.njk');
+
+  eleventyConfig.addFilter('sortByOrder', sortByOrder);
+
+  eleventyConfig.addCollection('allExceptSlides', (collectionApi) => {
+    return collectionApi
+      .getAll()
+      .filter((item) => !item.data.tags?.includes('slides'));
+  });
 
   return {
     dir: {
---
+++ pages/slides/owasp-top-ten/01-title.md
@@ -0,0 +1,5 @@
+---
+order: 1
+---
+
+# Owasp Top Ten
