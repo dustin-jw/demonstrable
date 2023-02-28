d27066da1250c5ad82273adf4de8a1881dd7fb4e
dustin-jw <dustin@heysparkbox.com>
2023-02-28 09:40:47 -0700

    chore: configure webc components for global use

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,12 @@
+const pluginWebc = require('@11ty/eleventy-plugin-webc');
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ 'src/public': '/' });
   eleventyConfig.addLayoutAlias('default', 'layout.njk');
+
+  eleventyConfig.addPlugin(pluginWebc, {
+    components: 'src/components/**/*.webc',
+  });
 
   return {
     dir: {
